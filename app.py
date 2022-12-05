34507acc5f990cdb327823f627203666bdc15687
emil-svensson <114189223+emil-svensson@users.noreply.github.com>
2022-12-05 15:38:08 -0500

    Update app.py
    minor changes

--- app.py
+++ app.py
@@ -9,9 +9,6 @@ from streamlit_folium import st_folium # https://github.com/randyzwitch/streamli
 
 st.set_page_config(layout="wide")
 
-st.title("Initial car registrations in Sweden")
-
-
 
 
 ## Import files
@@ -61,7 +58,7 @@ year_list = df_county['Date'].dt.year.sort_values().unique()
 #     x = "’"+ str(x)[-2:]    
 #     return x
 
-c_year = st.sidebar.select_slider('Select one year or a range:', year_list, value=[2014, 2021], 
+c_year = st.sidebar.select_slider('Select one year or a range:', year_list, value=[2013, 2021], 
                                 help='Drag the limits on top of each other for a single year', label_visibility='collapsed')
 
 st.sidebar.write(" ")
@@ -114,13 +111,11 @@ def add_color(df, col, colorscale):
 
 
 
-
-
-
 col1, col2 = st.columns(2)
 
 with col1:
-    
+    # st.title("Initial car registrations in Sweden")
+    st.markdown("# Initial car registrations in Sweden")
 
     # filter and aggregate data according to the user selected range
 
@@ -155,9 +150,9 @@ with col1:
 
 
 
-    map = folium.Map(location=[sample_county['County Latitude'].quantile(0.4), 
-                            sample_county['County Longitude'].quantile(0.5)], 
-                            zoom_start=6, 
+    map = folium.Map(location=[sample_county['County Latitude'].quantile(0.8), 
+                            sample_county['County Longitude'].quantile(0.45)],
+                            zoom_start=5, 
                             control_scale=True,
                             tiles="cartodbpositron")
                             # tiles="cartodbdark_matter")
@@ -169,7 +164,7 @@ with col1:
         [rowdata["County Latitude"], rowdata["County Longitude"]],
         radius=6,
         fill=True,
-        popup=rowdata["County"] +' '+ str(round(rowdata[col],1)),
+        tooltip = rowdata["County"] +' '+ str(round(rowdata[col],1)),
         color= rowdata['color']
         ).add_to(map)
 
@@ -190,96 +185,13 @@ with col1:
         st.markdown(f"{c_fuel} ({sample_county['Year'][0]}) $~~~~$ Minimum: **{round(sample_county[col].min(),1)} - {round(lq,1)}** $~~~~$ Maximum: **{round(uq,1)} - {round(sample_county[col].max(),1)}**", unsafe_allow_html=False)
 
     county_selected = None
-    map_selection = st_folium(map, width=600, height=600)     
+    map_selection = st_folium(map, width=650, height=520)
+    st.write("""Inital car registration rates are especially high in the large city regions
+    (Stockholm, Göteborg & Malmö), all located in the southern half of the country""")
     
 
 
-
-# Create new column with groups of fueltypes
-fuel_cat = {'Diesel':'Fossil',
-            'Gas':'Fossil',
-            'Electricity':'Electrified',
-            'Electric hybrids':'Electrified',
-            'Plug-in hybrids':'Electrified',
-            'Ethanol':'Other',
-            'NG':'Other',
-            'Others':'Other'}
-
-df_fuel['Category'] = df_fuel['Fuel type'].apply(lambda x: fuel_cat[x])
-
-# Add rows with all fuel types summed
-df_fuel_all = pd.DataFrame(df_fuel.groupby('Date', as_index=False).agg({'Count':'sum'}))
-df_fuel_all['Fuel type'] = 'All'
-df_fuel_all['Category'] = 'All'
-df_fuel = pd.concat([df_fuel, df_fuel_all], axis=0).sort_values(by='Date')
-
-# pre-aggregate per category and date
-df_fuel_agg = df_fuel.groupby(['Date', 'Category'], as_index=False).agg({'Count':'sum'})
-
-
-
-
-
-# change the altair x-axis aggregation based on the selected year range in the sidebar
-if c_year[1]-c_year[0] >=4:
-    # sum over years
-    agg_x = 'year(Date):T'
-    if c_year[0] > 2013:
-        df_fuel_agg = df_fuel_agg[(df_fuel_agg['Date'].dt.year >= c_year[0]) & (df_fuel_agg['Date'].dt.year <= c_year[1])]
-    else:
-        df_fuel_agg = df_fuel_agg[df_fuel_agg['Date'].dt.year <= c_year[1]]
-
-    # xaxisvalues = df_fuel_agg.date.dt.year.unique()
-
-elif (c_year[1]-c_year[0] < 4) & (c_year[1]-c_year[0] >=1):
-    # sum over years and quarter
-    agg_x = 'yearquarter(Date):T'
-    df_fuel_agg = df_fuel_agg[(df_fuel_agg['Date'].dt.year >= c_year[0]) & (df_fuel_agg['Date'].dt.year <= c_year[1])]
-    # xaxisvalues = pd.date_range(df_fuel_agg['date'].min(), df_fuel_agg['date'].max(), (c_year[1]-c_year[0])*4+1)
-
-else:
-    # no aggregation
-    agg_x = 'Date:T'
-    df_fuel_agg = df_fuel_agg[df_fuel_agg['Date'].dt.year == c_year[0]]
-
-agg_y = 'mean(Count):Q'
-
-
-
-
 with col2:
-
-
-    all_categories_lines = alt.Chart(df_fuel_agg, height=250).mark_line(
-    opacity=1).encode(
-        alt.X(agg_x, axis = alt.Axis(title='')), alt.Y('mean(Count):Q'), color='Category').transform_filter(
-        alt.FieldOneOfPredicate(field='Category', oneOf=['Electrified', 'Fossil', 'Other']))
-
-    electrified_points = alt.Chart(df_fuel_agg).transform_filter((datum.Category =='Electrified')).mark_point(
-        filled=True, size=40).encode(
-        alt.X(agg_x, axis=alt.Axis(title='')), alt.Y('mean(Count):Q', axis=alt.Axis(title='Average per month')))
-
-    grandtotal_area = alt.Chart(df_fuel_agg).transform_filter((datum.Category =='All')).mark_area(
-        color='lightgray', opacity=0.2).encode(
-        alt.X(agg_x, axis=alt.Axis(title='')), alt.Y('mean(Count):Q', axis=alt.Axis(title='Average per month')))
-
-    grandtotal_line = alt.Chart(df_fuel_agg).transform_filter((datum.Category =='All')).mark_line(
-    color='lightgray', opacity=0.5).encode(
-    alt.X(agg_x, axis=alt.Axis(title='')), alt.Y('mean(Count):Q', axis=alt.Axis(title='Average per month')))
-
-    fuelcategories_chart = all_categories_lines + electrified_points + grandtotal_area + grandtotal_line
-
-
-    st.markdown('##### Monthly national averages', unsafe_allow_html=False)
-    st.write("""2021 was the first year electrified cars (pure EVs, electric & plug-in hybrids) 
-surpassed gas and diesel powered vehicles in terms of initial registrations. 
-Over the shown 16 years, the total number of registrations per year is fluctuating significantly. 
-The low in 2008 & 2009 coincides with effects of the global finacial crisis fully unravelling in Sweden.""")
-    st.write(' ')
-    st.write(' ')
-    st.altair_chart(fuelcategories_chart.interactive(), use_container_width=True)
-    st.write(' ')
-
 
     df_selected_county = pd.read_csv('./data/tempdata.csv')
     county_selected = df_selected_county.iloc[0]['County']
@@ -307,5 +219,86 @@ The low in 2008 & 2009 coincides with effects of the global finacial crisis full
         alt.X('year(Date):T', axis = alt.Axis(title='')), alt.Y(yval, axis=alt.Axis(title=yaxistitle)), color='Fuel type').transform_filter(
         alt.FieldOneOfPredicate(field='Fuel type', oneOf=['Electricty', 'Electric hybrids', 'Plug-in hybrids', 'Gas', 'Diesel', 'NG', 'Ethanol']))
 
+    st.write(" ")
+    st.write(" ")
     st.markdown(f'**{county_selected}**: Yearly registrations', unsafe_allow_html=False)
     st.altair_chart(linechart_one_county.interactive(), use_container_width=True)
+
+
+# Create new column with groups of fueltypes
+fuel_cat = {'Diesel':'Fossil',
+            'Gas':'Fossil',
+            'Electricity':'Electrified',
+            'Electric hybrids':'Electrified',
+            'Plug-in hybrids':'Electrified',
+            'Ethanol':'Other',
+            'NG':'Other',
+            'Others':'Other'}
+
+df_fuel['Category'] = df_fuel['Fuel type'].apply(lambda x: fuel_cat[x])
+
+# Add rows with all fuel types summed
+df_fuel_all = pd.DataFrame(df_fuel.groupby('Date', as_index=False).agg({'Count':'sum'}))
+df_fuel_all['Fuel type'] = 'All'
+df_fuel_all['Category'] = 'All'
+df_fuel = pd.concat([df_fuel, df_fuel_all], axis=0).sort_values(by='Date')
+
+# pre-aggregate per category and date
+df_fuel_agg = df_fuel.groupby(['Date', 'Category'], as_index=False).agg({'Count':'sum'})
+
+
+
+
+
+# define variable yearrange based on the selected year range in the sidebar and filter data
+if c_year[0] == 2013:
+    yearrange = c_year[1]-2005
+    df_fuel_agg = df_fuel_agg[df_fuel_agg['Date'].dt.year <= c_year[1]]
+else:
+    yearrange = c_year[1]-c_year[0]
+    df_fuel_agg = df_fuel_agg[(df_fuel_agg['Date'].dt.year >= c_year[0]) & (df_fuel_agg['Date'].dt.year <= c_year[1])]
+
+
+# change the altair x-axis aggregation based on yearrange
+if yearrange >=4:
+    agg_x = 'year(Date):T' # sum over years
+elif (yearrange < 4) & (yearrange >=1):
+    agg_x = 'yearquarter(Date):T'# sum over years and quarter
+else:
+    agg_x = 'Date:T' # no aggregation
+
+
+
+
+
+with col2:
+
+    all_categories_lines = alt.Chart(df_fuel_agg, height=250).mark_line(
+    opacity=1).encode(
+        alt.X(agg_x, axis = alt.Axis(title='')), alt.Y('mean(Count):Q'), color='Category').transform_filter(
+        alt.FieldOneOfPredicate(field='Category', oneOf=['Electrified', 'Fossil', 'Other']))
+
+    electrified_points = alt.Chart(df_fuel_agg).transform_filter((datum.Category =='Electrified')).mark_point(
+        filled=True, size=40).encode(
+        alt.X(agg_x, axis=alt.Axis(title='')), alt.Y('mean(Count):Q', axis=alt.Axis(title='Average per month')))
+
+    grandtotal_area = alt.Chart(df_fuel_agg).transform_filter((datum.Category =='All')).mark_area(
+        color='lightgray', opacity=0.2).encode(
+        alt.X(agg_x, axis=alt.Axis(title='')), alt.Y('mean(Count):Q', axis=alt.Axis(title='Average per month')))
+
+    grandtotal_line = alt.Chart(df_fuel_agg).transform_filter((datum.Category =='All')).mark_line(
+    color='lightgray', opacity=0.5).encode(
+    alt.X(agg_x, axis=alt.Axis(title='')), alt.Y('mean(Count):Q', axis=alt.Axis(title='Average per month')))
+
+    fuelcategories_chart = all_categories_lines + electrified_points + grandtotal_area + grandtotal_line
+
+
+    st.markdown('##### Monthly national averages (smoothed)', unsafe_allow_html=False)
+    st.write("""2021 was the first year electrified cars (pure EVs, electric & plug-in hybrids) 
+surpassed gas and diesel powered vehicles in terms of initial registrations. 
+Over the shown 16 years, the total number of registrations per year is fluctuating significantly. 
+The low in 2008 & 2009 coincides with effects of the global finacial crisis fully unravelling in Sweden.""")
+    st.write(' ')
+    st.write(' ')
+    st.altair_chart(fuelcategories_chart.interactive(), use_container_width=True)
+    st.write(' ')
